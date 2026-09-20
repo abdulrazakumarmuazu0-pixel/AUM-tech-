fae64e96f0e04858f92bbbb9f8fcb4695389e2bf
abdulrazakumarmuazu0-pixel <abdulrazakumarmuazu0@gmail.com>
2026-09-20 01:08:49 +0100

    Fix Super Admin script loading and admin entry-point redirect

---
+++ admin/js/super.js
@@ -0,0 +1,3 @@
+// Compatibility bridge for the Super Admin page.
+// The implementation lives in admin/JS/super.js while super.html loads js/super.js.
+document.write('<script src="../JS/super.js"><\/script>');
